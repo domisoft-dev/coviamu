93a2a8926964c85a172a31fd4d1111ed85308df0
Ian <maceachen16@gmail,com>
2025-07-22 22:45:13 -0300

    DDL sql estructuras y dump

---
+++ sql/ddl.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE Domisoft;
+USE Domisoft;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    email VARCHAR(100),
+    contrasena VARCHAR(100),
+    estado VARCHAR(100)
+);
+CREATE TABLE admins (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(100),
+  contrasena VARCHAR(100)
+);
+
+INSERT INTO users (nombre, email, contrasena, estado)
+VALUES
+('Laura', 'lauragomez@gmail.com', 'Laurita123', 'no_aprobado'),
+('Felipe', 'felipepro@gmail.com', 'siempreborro2', 'no_aprobado'),
+('Marta', 'marta_lopez@hotmail.com', '124567890', 'aprobado');
+
+INSERT INTO admins (nombre, contrasena)
+VALUES 
+('Ian', '222'),
+('Samuel', '123'),
+('Renzo', '111'),
+('Samuel', '333');
