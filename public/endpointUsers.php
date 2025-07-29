e89944ed0641adee27db9906814b83fcdef57cc8
ian <76226935+ianrtt@users.noreply.github.com>
2025-07-29 18:54:09 -0300

    Arreglos bugs endpointUsers.php

--- public/endpointUsers.php
+++ public/endpointUsers.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
-require_once '../api/controllers/controladorUsuario.php';
-
 header('Content-Type: application/json');
+require_once '../api/controllers/controladorUsuario.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 $data = json_decode(file_get_contents("php://input"), true);
+if (!$data) {
+    $data = $_REQUEST;
+}
 
 if ($method !== 'POST') {
     http_response_code(405);
