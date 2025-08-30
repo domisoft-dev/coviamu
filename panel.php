c1ed6d9797ee867198d09d9c39162f0732d5039a
ian <maceachen16@gmail.com>
2025-08-30 20:58:03 -0300

    fix: re-design&fusion-css

--- panel.php
+++ panel.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['admin'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel de Gestion</title>
-    <link rel="stylesheet" href="public/css/panel.css">
+    <link rel="stylesheet" href="public/css/index.css">
 </head>
 <body>
 
